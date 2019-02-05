7ab91518c2127aacfe69c9c302176fbd437e7efd
github username <github email address>
2019-02-04 19:32:24 -0500

    some basic ui

--- src/components/memberPage.js
+++ src/components/memberPage.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import EventTop from './eventTop.js'
-
+import MembersSidebar from './membersSidebar.js'
 class memberPage extends Component{
 
   state={
-    eventData: []
+    eventData: [],
+    members: []
   }
 
   event = () => {
     fetch('http://localhost:3000/api/v1/groups')
     .then(res => res.json())
     .then(data => {
-      console.log(data[0].events)
+      console.log(data[0].volounteers)
       this.setState({
-        eventData: data[0].events
+        eventData: data[0].events,
+        members: data[0].volounteers
       })
     })
   }
@@ -25,11 +27,11 @@ class memberPage extends Component{
 
 render(){
   return(
-
-    <div class="container">
-  	<div class="row">
-  		<div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
-              <div class="MultiCarousel-inner">
+    <div>
+    <div className="container">
+  	<div className="row">
+  		<div className="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
+              <div className="MultiCarousel-inner">
             {this.state.eventData.map(g => <EventTop groupData={g} key={g.id}/>)}
             </div>
             <button className="btn btn-primary leftLst"></button>
@@ -37,6 +39,21 @@ render(){
             </div>
             </div>
             </div>
+            <div className="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation" >
+              <div className="well sidebar-nav">
+              <div className="container">
+            	<div className="row">
+            		<div className="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
+                        <div className="MultiCarousel-inner">
+             {this.state.members.map(g => <MembersSidebar groupData={g} key={g.id}/>)}
+              </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+
   )
 }
 
